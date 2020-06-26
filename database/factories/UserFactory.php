d34b442dac6597f41c8386901fdbe12d0ee6f21a
Reza Khan <rezakhan995@gmail.com>
2020-06-27 00:12:10 +0600

    factory updated for adding dummy data using faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,6 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -17,12 +16,29 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-    ];
-});
+    $factory->define(App\User::class, function (Faker $faker) {
+        return [
+            'name' => $faker->name,
+            'email' => $faker->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ];
+    });
+
+    $factory->define(App\Category::class, function (Faker $faker) {
+        return [
+            'cat_name' => $faker->sentence,
+        ];
+    });
+
+    $factory->define(App\Post::class, function (Faker $faker) {
+        return [
+            'user_id' => rand(1, 10),
+            'cat_id' => rand(1, 10),
+            'comment_id' => rand(1, 10),
+            'title' => $faker->sentence,
+            'photo' => $faker->imageUrl,
+            'description' => $faker->paragraph,
+        ];
+    });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Category;
+use App\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        factory(User::class, 10)->create();
+        factory(Category::class, 10)->create();
+        factory(Post::class, 10)->create();
     }
 }
